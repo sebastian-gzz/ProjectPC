870e561e8d4bda46327ab3e2367b95c0d45128a0
sebastian-gzz <70810919+sebastian-gzz@users.noreply.github.com>
2020-09-11 16:31:52 -0500

    Add files via upload

---
+++ E2SGM.py
@@ -0,0 +1,33 @@
+import requests
+import os
+import sys
+from bs4 import BeautifulSoup as bs
+import webbrowser
+
+print("Este script navega en las páginas de noticas de la UANL")
+inicioRango = int(input("Pagina inicial para buscar: "))
+finRango = int(input("Pagina final para buscar: "))
+dependencia = input("Ingrese las siglas de la Facultad a buscar: ")
+if inicioRango > finRango:
+    inicioRango,finRango = finRango,inicioRango
+for i in range (inicioRango,finRango,1):
+    url = "https://www.uanl.mx/noticias/page/"+str(i)
+    pagina = requests.get (url)
+    if pagina.status_code != 200:
+        raise TypeError("Pagina no encontrada")
+    else:
+        soup = bs(pagina.content,"html.parser")
+        info = soup.select("h3 a")
+        for etiqueta in info:
+            url2 = etiqueta.get("href")
+            pagina2 = requests.get(url2)
+            if pagina2.status_code == 200:
+                soup2 = bs(pagina2.content,"html.parser")
+                parrafos = soup2.select("p")    
+                for elemento in parrafos:
+                    if dependencia in elemento.getText():
+                        print ("Abriendo",url2)
+                        webbrowser.open(url2)
+                        break
+    
+
